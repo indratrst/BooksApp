12fa62838ed20ea56cf1ad5e8a823e4a04179813
indratrst <tristia71@gmail.com>
2023-02-06 17:25:51 +0700

    edit button

--- main/main.js
+++ main/main.js
@@ -22,32 +22,25 @@ function functCheck() {
 	}
 }
 function addBook() {
-	const textTitle = document.getElementById("txtTitle").value;
-	const textAuthor = document.getElementById("txtAuthor").value;
+	let id = +new Date();
+	const title = document.getElementById("txtTitle").value;
+	const author = document.getElementById("txtAuthor").value;
 	const year = document.getElementById("txtYear").value;
 	const isComplete = document.getElementById("inputBookIsComplete").checked;
-
-	const generatedID = generateId();
-	const bookObject = generateBookObject(generatedID, textTitle, textAuthor, year, isComplete);
-	bookShelf.push(bookObject);
-
-	document.dispatchEvent(new Event(RENDER_EVENT));
-	saveData();
-}
-
-function generateId() {
-	return +new Date();
-}
-
-function generateBookObject(id, title, author, year, isComplete) {
-	return {
+	const bookObject = {
 		id,
 		title,
 		author,
 		year,
 		isComplete,
-	};
+	}
+	bookShelf.push(bookObject);
+	// console.log(bookObject);
+	document.dispatchEvent(new Event(RENDER_EVENT));
+	saveData();
 }
+
+
 
 document.addEventListener(RENDER_EVENT, function () {
 	const uncompletedList = document.getElementById("incompleteBookshelfList");
@@ -63,6 +56,7 @@ document.addEventListener(RENDER_EVENT, function () {
 	}
 });
 
+
 function makeBook(bookObject) {
 	const txtTitle = document.createElement("h3");
 	txtTitle.innerText = bookObject.title;
@@ -73,24 +67,30 @@ function makeBook(bookObject) {
 	const txtYear = document.createElement("p");
 	txtYear.innerText = "Tahun : " + bookObject.year;
 
-	// const shadow = document.createElement("div");
-	// shadow.classList.add("container-card");
-
 	const action = document.createElement("div");
 	action.classList.add("action");
 
 	const container = document.createElement("div");
 	container.classList.add("dataBook");
 	container.append(txtTitle, textAuthor, txtYear, action);
-	// shadow.append(action);
 
-	action.setAttribute("id", `book-${bookObject.id}`);
+	action.setAttribute("id", `${bookObject.id}`);
+
 	const imgRemove = document.createElement("img");
 	imgRemove.src = "assets/trash.png";
 
+	action.setAttribute("id",
+		`${bookObject.id}`,
+		`${bookObject.author}`,
+		`${bookObject.year}`,
+		`${bookObject.title}`);
+
+	const editIcon = document.createElement("img");
+	editIcon.src = "assets/sync.png";
+
 	if (bookObject.isComplete) {
 		const imgMove = document.createElement("img");
-		imgMove.src = "assets/sync.png";
+		imgMove.src = "assets/check-mark.png";
 		const notFinishButton = document.createElement("button");
 		notFinishButton.classList.add("move");
 		notFinishButton.append(imgMove);
@@ -109,12 +109,19 @@ function makeBook(bookObject) {
 			delToast();
 		});
 
-		action.append(notFinishButton, removeButton);
+		const editBtn = document.createElement("button");
+		editBtn.classList.add("edit");
+		editBtn.append(editIcon);
+		editBtn.addEventListener("click", function () {
+			editBook(bookObject.id);
+
+		});
+
+		action.append(notFinishButton, removeButton, editBtn);
 	} else {
 		const trashButton = document.createElement("button");
 		trashButton.classList.add("remove");
 		trashButton.append(imgRemove);
-
 		trashButton.addEventListener("click", function () {
 			removeTaskFromCompleted(bookObject.id);
 			delToast();
@@ -130,14 +137,72 @@ function makeBook(bookObject) {
 			moveToast();
 		});
 
-		action.append(finishButton, trashButton);
+
+		const editBtn = document.createElement("button");
+		editBtn.classList.add("edit");
+		editBtn.append(editIcon);
+		editBtn.addEventListener("click", function () {
+			editBook(bookObject.id);
+		});
+
+
+		action.append(finishButton, trashButton, editBtn);
 	}
 	return container;
 }
 
+
+function editBook(bookId) {
+	const bookTarget = findBook(bookId);
+	//menampilkan data buku di form edit
+
+	const editBookTitle = document.getElementById("txtTitle");
+	const editBookAuthor = document.getElementById("txtAuthor");
+	const editYear = document.getElementById("txtYear");
+	const editIsComplete = document.getElementById("inputBookIsComplete");
+	editBookTitle.value = bookTarget.title;
+	editBookAuthor.value = bookTarget.author;
+	editYear.value = bookTarget.year;
+	editIsComplete.checked = bookTarget.isComplete;
+	const submitEdit = document.getElementById("btn-edit");
+	submitEdit.addEventListener("click", function (event) {
+		// memasukan data baru pada buku yang diedit
+		updateEditBook(
+			editBookTitle.value,
+			editBookAuthor.value,
+			editYear.value,
+			editIsComplete.checked,
+			bookId
+		);
+		event.preventDefault();
+	});
+}
+// update data buku di local storage
+function updateEditBook(title, author, year, isComplete, id) {
+	// mengambil data pada local storage dan dikonversi dari String menjadi Objek
+	const StorageBook = JSON.parse(localStorage[STORAGE_KEY]);
+	const bookIndex = findBookIndex(id);
+	console.log(StorageBook);
+	console.log(bookIndex);
+	// membentuk data baru pada buku
+	StorageBook[bookIndex] = {
+		id: id,
+		title: title,
+		author: author,
+		year: year,
+		isComplete: isComplete,
+	};
+
+	// konversi data menjadi String
+	const parsed = JSON.stringify(StorageBook);
+	localStorage.setItem(STORAGE_KEY, parsed);
+	// memuat halaman setelah data diubah
+	location.reload(true);
+}
+
 function addTaskToCompleted(bookId) {
 	const bookTarget = findBook(bookId);
-
+	console.log(bookTarget);
 	if (bookTarget == null) return;
 
 	bookTarget.isComplete = true;
@@ -145,8 +210,11 @@ function addTaskToCompleted(bookId) {
 	saveData();
 }
 
+
+
 function undoTaskFromCompleted(bookId) {
 	const bookTarget = findBook(bookId);
+	console.log(bookTarget);
 
 	if (bookTarget == null) return;
 
@@ -174,6 +242,9 @@ function removeTaskFromCompleted(bookId) {
 	saveData();
 }
 
+
+
+
 function findBookIndex(bookId) {
 	for (const index in bookShelf) {
 		if (bookShelf[index].id === bookId) {
@@ -200,12 +271,18 @@ function saveData() {
 	}
 }
 
+
+
+
 const SAVED_EVENT = "saved-book";
 const STORAGE_KEY = "BOOKS_APPS";
 
 function loadDataFromStorage() {
 	const serializedData = localStorage.getItem(STORAGE_KEY);
+	// console.log(serializedData);
+
 	let data = JSON.parse(serializedData);
+	// console.log(data);
 
 	if (data !== null) {
 		for (const book of data) {
@@ -216,7 +293,7 @@ function loadDataFromStorage() {
 	document.dispatchEvent(new Event(RENDER_EVENT));
 }
 
-function delToast() {
+const delToast = () => {
 	const removeToast = document.getElementById("removeToast");
 	removeToast.innerText = "Berhasil menghapus item";
 	removeToast.className = "show";
@@ -225,7 +302,7 @@ function delToast() {
 	}, 1000);
 }
 
-function moveToast() {
+const moveToast = () => {
 	let moveToast = document.getElementById("moveToast");
 	moveToast.innerText = "Berhasil memindahkan item";
 	moveToast.className = "show";
@@ -234,7 +311,7 @@ function moveToast() {
 	}, 1000);
 }
 
-function searchData() {
+const searchData = () => {
 	let input, filter, table, dataBook, h3, txtValue, form, incompBook, compBook;
 	incompBook = document.getElementById("incomplete-book");
 	compBook = document.getElementById("complete-book");
@@ -277,14 +354,3 @@ function searchData() {
 	}
 }
 
-// const btnSearch = document.getElementById("searchSubmit");
-// btnSearch.addEventListener("click", (event) => {
-//   searchData();
-// });
-
-// const search = document.getElementById("searchBookTitle");
-// search.addEventListener("keyup", (event) => {
-//   if (event.key === "Enter") {
-//     searchData();
-//   }
-// });
